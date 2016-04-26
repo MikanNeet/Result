41c0d60a8fbb234551cc78862e5c655f484bd967
yzcc <kaizoku_1113@yahoo.co.jp>
2016-04-26 21:55:28 +0900

    Added files via upload

---
+++ Result_Manager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Result_Manager : MonoBehaviour {
+
+
+    public GameObject block;
+    public GameObject shooting;
+    public GameObject typing;
+
+
+	// Use this for initialization
+	void Start () {
+        if (GameObject.Find("DeriverPoint").GetComponent<B_StageNum>().game_name == "block")
+        {
+            Instantiate(block, block.transform.position, block.transform.rotation);
+        }
+        else if (GameObject.Find("DeriverPoint").GetComponent<B_StageNum>().game_name == "Kirie_Shooting")
+        {
+            Instantiate(shooting, shooting.transform.position, shooting.transform.rotation);
+        }
+        else if (GameObject.Find("DeriverPoint").GetComponent<B_StageNum>().game_name == "typing")
+        {
+            Instantiate(typing, typing.transform.position, typing.transform.rotation);
+        }
+    }
+	
+}
---
+++ Result_Text.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Result_Text : MonoBehaviour {
+
+    public Text[] text = new Text[4];
+    public Text text_all;
+    public Text umr;
+    GameObject point;
+    GameObject money;
+
+    bool move;
+    // Use this for initialization
+    void Start () {
+        point = GameObject.Find("DeriverPoint");
+        money = GameObject.Find("money");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //まだまとめられるけどとりあえず全体のステージ数増えるかもしんないからこのまんま
+        //シルフィン
+        if (point.GetComponent<B_StageNum>().game_name == "block")
+        {
+            text[0].text = "1st:" + point.GetComponent<B_StageNum>().score[0];
+            text[1].text = "2nd:" + point.GetComponent<B_StageNum>().score[1];
+            text[2].text = "3rd:" + point.GetComponent<B_StageNum>().score[2];
+            text_all.text = "Total:" + point.GetComponent<B_StageNum>().since_score_;
+            umr.text = "Money:" + money.GetComponent<Umr_Coin>().getMoney();
+            if (!move && Input.GetMouseButtonDown(0))
+            {
+                move = true;
+                point.GetComponent<B_StageNum>().Dec(point.GetComponent<B_StageNum>().since_score_);
+                money.GetComponent<Umr_Coin>().Inc(point.GetComponent<B_StageNum>().since_score_ + 1);
+            }
+            else if (move && Input.GetMouseButtonDown(0))
+            {
+                point.GetComponent<B_StageNum>().Death();
+                Application.LoadLevel("Block_Menu");
+            }
+        }
+        //きりえ
+        else if (point.GetComponent<B_StageNum>().game_name == "Kirie_Shooting")
+        {
+            text[0].text = "1st:" + point.GetComponent<B_StageNum>().score[0];
+            text[1].text = "2nd:" + point.GetComponent<B_StageNum>().score[1];
+            text[2].text = "3rd:" + point.GetComponent<B_StageNum>().score[2];
+            text[3].text = "4th:" + point.GetComponent<B_StageNum>().score[3];
+            text_all.text = "Total:" + point.GetComponent<B_StageNum>().since_score_;
+            umr.text = "Money:" + money.GetComponent<Umr_Coin>().getMoney();
+
+            if (!move && Input.GetMouseButtonDown(0))
+            {
+                move = true;
+                point.GetComponent<B_StageNum>().Dec(point.GetComponent<B_StageNum>().since_score_);
+                money.GetComponent<Umr_Coin>().Inc(point.GetComponent<B_StageNum>().since_score_ + 1);
+            }
+            else if (move && Input.GetMouseButtonDown(0))
+            {
+                point.GetComponent<B_StageNum>().Death();
+                //Application.LoadLevel("Block_Menu");
+            }
+        }
+        //海老菜
+        else if (point.GetComponent<B_StageNum>().game_name == "typing")
+        {
+            text[0].text = "1st:" + point.GetComponent<B_StageNum>().score[0];
+            text[1].text = "2nd:" + point.GetComponent<B_StageNum>().score[1];
+            text[2].text = "3rd:" + point.GetComponent<B_StageNum>().score[2];
+            text[3].text = "4th:" + point.GetComponent<B_StageNum>().score[3];
+            text_all.text = "Total:" + point.GetComponent<B_StageNum>().since_score_;
+            umr.text = "Money:" + money.GetComponent<Umr_Coin>().getMoney();
+            if (!move && Input.GetMouseButtonDown(0))
+            {
+                move = true;
+                point.GetComponent<B_StageNum>().Dec(point.GetComponent<B_StageNum>().since_score_);
+                money.GetComponent<Umr_Coin>().Inc(point.GetComponent<B_StageNum>().since_score_ + 1);
+            }
+            else if (move && Input.GetMouseButtonDown(0))
+            {
+                point.GetComponent<B_StageNum>().Death();
+                Application.LoadLevel("Main");
+            }
+        }
+    }
+}
